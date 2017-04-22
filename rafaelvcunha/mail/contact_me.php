9c43fa6184fee12a398caca6cc3cbe2c70b126dc
rafaelvcunha <rafaelvcunha@gmail.com>
2017-04-22 00:20:08 -0300

    criando index

--- rafaelvcunha/mail/contact_me.php
+++ rafaelvcunha/mail/contact_me.php
@@ -17,9 +17,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'rafaelvcunha@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@rafaelvcunha.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_subject = "Novo contato com rafaelvcunha:  $name ";
+$email_body = "Você recebeu um novo contato pelo site rafaelvcunha.\n\n"."Aqui estão mais detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nCelular: $phone\n\nMensagem:\n\n$message";
+$headers = "From: noreply@rafaelvcunha.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
