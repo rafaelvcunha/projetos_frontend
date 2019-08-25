d956df854c89f89e4dd134b8a085ac9a949962df
rafaelvcunha <rafaelvcunha@gmail.com>
2019-08-25 07:23:03 -0300

    criado functions para formatação de textos

--- Javascript_treinamentos/JavascriptApp/js/main.js
+++ Javascript_treinamentos/JavascriptApp/js/main.js
@@ -1,7 +1,7 @@
 var list = [
-    {"desc":"Arroz", "amount":"1", "value":"5.40"},
-    {"desc":"Carne", "amount":"12", "value":"15.40"},
-    {"desc":"Bolo", "amount":"3", "value":"8.40"}
+    {"desc":"arRoz", "amount":"1", "value":"5.40"},
+    {"desc":"caRne", "amount":"12", "value":"15.40"},
+    {"desc":"boLo", "amount":"3", "value":"8.40"}
 ];
 
 function getTotal(list){
@@ -15,10 +15,22 @@ function getTotal(list){
 function setList(list){
     var table = '<thead><tr><th scope="col">Description</th><th scope="col">Amount</th><th scope="col">Value</th><th scope="col">Action</th></tr></thead><tbody>';
     for(var key in list){
-        table += '<tr><th scope="row">'+ list[key].desc +'</th><td>'+ list[key].amount +'</td><td>'+ list[key].value+'</td><td> Edit | Delete </td></tr>';
+        table += '<tr><th scope="row">'+ formatDesc(list[key].desc) +'</th><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td> Edit | Delete </td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
+}
+
+function formatDesc(desc){
+    var str = desc.toLowerCase();
+    str = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    return str;
+}
+
+function formatValue(val){
+    var str = parseFloat(val).toFixed(2) + "";
+    str = "R$ " + str.replace(".", ",");
+    return str;
 }
 
 setList(list);
