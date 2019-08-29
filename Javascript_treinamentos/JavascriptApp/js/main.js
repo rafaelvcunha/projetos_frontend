73b6bd56d8557e2a38404811898f25b5edb26da8
rafaelvcunha <rafaelvcunha@gmail.com>
2019-08-29 00:48:38 -0300

    implementado localStorage e treinamento finalizado

--- Javascript_treinamentos/JavascriptApp/js/main.js
+++ Javascript_treinamentos/JavascriptApp/js/main.js
@@ -9,16 +9,19 @@ function getTotal(list){
     for( var key in list){
         total += list[key].value * list[key].amount;
     }
-    return total;
+
+    document.getElementById("totalValue").innerHTML = formatValue(total);
 }
 
 function setList(list){
     var table = '<thead><tr><th scope="col">Description</th><th scope="col">Amount</th><th scope="col">Value</th><th scope="col">Action</th></tr></thead><tbody>';
     for(var key in list){
-        table += '<tr><th scope="row">'+ formatDesc(list[key].desc) +'</th><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button onclick="setUpdate('+key+');" class="btn btn-default" >Edit</button> <button onclick="deleteData('+key+');" class="btn btn-default" >Delete</button> </td></tr>';
+        table += '<tr><th scope="row">'+ formatDesc(list[key].desc) +'</th><td>'+ formatAmount(list[key].amount) +'</td><td>'+ formatValue(list[key].value) +'</td><td><button onclick="setUpdate('+key+');" class="btn btn-default" >Edit</button> <button onclick="deleteData('+key+');" class="btn btn-default" >Delete</button> </td></tr>';
     }
     table += '</tbody>';
     document.getElementById("listTable").innerHTML = table;
+    getTotal(list);
+    saveListStorage(list);
 }
 
 function formatDesc(desc){
@@ -33,7 +36,14 @@ function formatValue(val){
     return str;
 }
 
+function formatAmount(val){
+    return parseInt(val);
+}
+
 function addData(){
+    if(!validation()){
+        return
+    }
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
@@ -61,9 +71,13 @@ function resetForm(){
     document.getElementById("btnAdd").style.display = "inline-block";
 
     document.getElementById("inputIDUpdate").innerHTML = "";
+    document.getElementById("errors").style.display = "none";
 }
 
 function updateData(){
+    if(!validation()){
+        return
+    }
     var id = document.getElementById("idUpdate").value;
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
@@ -95,4 +109,60 @@ function deleteData(id){
 
 }
 
-setList(list);
+function validation(){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    var errors = "";
+
+    document.getElementById("errors").style.display = "none";
+    if (desc === ""){
+        errors += '<p>Fill out description</p>';
+    }
+    if (amount === ""){
+        errors += '<p>Fill out amount</p>';
+    } else if (amount != parseInt(amount)){
+        errors += '<p>Fill out valid amount</p>';
+    }
+    if (value === ""){
+        errors += '<p>Fill out value</p>';
+    } else if (value != parseFloat(value)){
+        errors += '<p>Fill out valid value</p>';
+    }
+
+    if (errors != ""){
+        document.getElementById("errors").style.display = "block";
+        document.getElementById("errors").style.backgroundColor = "rgba(85, 85, 85, 0.3)";
+        document.getElementById("errors").style.color = "white";
+        document.getElementById("errors").style.padding = "10px";
+        document.getElementById("errors").style.margin = "10px";
+        document.getElementById("errors").style.borderRadius = "13px";
+
+        document.getElementById("errors").innerHTML = "<h3>Error:</h3>" + errors;
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+function deleteList(){
+    if(confirm("Delete this list?")){
+        list = [];
+        setList(list);
+    }
+}
+
+function saveListStorage(list){
+    var jsonStr = JSON.stringify(list);
+    localStorage.setItem("list",jsonStr);
+}
+
+function initListStorage(){
+    var testList = localStorage.getItem("list");
+    if(testList){
+        list = JSON.parse(testList);
+    }
+    setList(list);
+}
+
+initListStorage();
